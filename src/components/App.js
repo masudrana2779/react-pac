98e804c35c3891ac7cd686a0d65293defeff1b32
Masud Rana <masudrana2779@gmail.com>
2020-02-16 12:36:25 +0600

    final

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from "./home/home";
+
+import  "../sass/style.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/home/home.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <center>
+                    <h1>This is Default React Pac</h1>
+                </center>
+            </div>
+        );
+    }
+}
+
+export default Home;
