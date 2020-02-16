cd5e2027baef9ecd2d1992cf2a6fa20844b97429
Masud Rana <masudrana2779@gmail.com>
2020-02-16 12:47:14 +0600

    final

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -3,7 +3,11 @@ import React, {Component} from 'react';
 class Home extends Component {
     render() {
         return (
-            <div>
+            <div style={{display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
+    width: "100%" }}>
                 <center>
                     <h1>This is Default React Pac</h1>
                 </center>
