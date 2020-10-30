c5e17bdaee3ce24c1373a8e1f6def041ba9bb6be
Masud Rana <masudrana2779@gmail.com>
2020-10-30 12:24:32 +0600

    test-react project added

---
+++ src/components/home/Jjjjj.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Jjjjj extends Component {
+    render() {
+        return (
+            <div>
+                <p>llcjlkjj asdkjjhd h</p>
+            </div>
+        )
+    }
+}
---
+++ src/utils/routes.js
@@ -0,0 +1,2 @@
+export const home = "/home";
+export const home2 = "/home2";
